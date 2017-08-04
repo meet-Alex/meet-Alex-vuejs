8e294a6a84d4533a2c69fea11f0ee358f8ca8cef
Piethein Strengholt <piethein@strengholt-online.nl>
2017-08-04 09:18:12 +0200

    commit initial project

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import Home from './components/Home.vue';
+import Test from './components/test/Test.vue';
+
+export const routes = [
+    { path: '/', component: Home },
+    { path: '/test', component: Test }
+];
