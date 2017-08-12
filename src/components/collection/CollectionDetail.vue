261a6b97b9587ee5d701768bb0782dc8d62b49a0
Hoogkamer <m.a.hoogkamer@gmail.com>
2017-08-12 09:33:29 +0200

    collections detail

--- src/components/collection/CollectionDetail.vue
+++ src/components/collection/CollectionDetail.vue
@@ -1,21 +1,57 @@
+<style>
+.compact{
+    display:table-cell;
+    white-space: nowrap;
+    overflow:hidden;
+    text-overflow: ellipsis;
+    width: auto;
+}
+.table{
+    margin:0;
+    padding:0;
+    display:table;
+    table-layout: fixed;
+    width:100%;
+    max-width:100%;
+}
+.name{
+    width:150px;
+       
+  width: 150px;
+}
+.table>tbody>tr>td {
+   padding:2px;
+}
+
+
+</style>
+
+
+
 <template>
     <div class="container">
         <ul class="breadcrumb breadcrumb-section">
             <li><router-link :to="{ name: 'Home' }">Home</router-link></li>
             <router-link to="/collections" activeClass="active" tag="li"><a>Collections</a></router-link>
             <li class="active">{{ collection.collection_name }}</li>
+            <li class="active"> Terms </li>
         </ul>
 
-        <h4>Collections detail</h4>
-        <h5>{{ $route.params.id }}</h5>
+        <table class="table table-striped table-bordered">
+                  
+                  <tbody>
+                      <tr class="" v-for="term in collection.terms">
+                          <td class="name">
+                              {{ term.term_name }}
+                          </td>
+                          <td class="compact">
+                              {{ term.term_definition }}
+                          </td>
+                      </tr>
+                  </tbody>
+                </table>
 
-        <div class="text-center">
-            <ul class="pagination">
-                <li v-for="letter in letters">
-                    <a>{{ letter }}</a>
-                </li>
-            </ul>
-        </div>
+       
     </div>
 </template>
 
@@ -38,11 +74,12 @@
                     return response.json();
                 })
                 .then (data => {
+                    console.log(data);
                     this.collection = data;
                 });
             },
             fetchCollectionLetters: function() {
-                this.$http.get('collections/' + this.$route.params.id + '/letters')
+                this.$http.get('collections/' + this.$route.params.id)
                 .then(response => {
                     return response.json();
                 })
--- src/main.js
+++ src/main.js
@@ -7,7 +7,12 @@ import { routes } from './routes';
 Vue.use(VueRouter);
 Vue.use(VueResource);
 
-Vue.http.options.root = 'http://localhost/meet-alex/public/index.php/api/';
+/*Vue.http.options.root = 'http://localhost/meet-alex/public/index.php/api/';*/
+
+
+
+
+Vue.http.options.root = 'https://www.meet-alex.org/index.php/api/';
 
 const router = new VueRouter({
     mode: 'history',
@@ -15,7 +20,7 @@ const router = new VueRouter({
 });
 
 new Vue({
-  el: '#app',
-  router,
-  render: h => h(App)
+    el: '#app',
+    router,
+    render: h => h(App)
 });
