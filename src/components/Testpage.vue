de7feaba6a13435f4423d74b1284420d4ad43c6a
Hoogkamer <HO0384@launcher.int.abnamro.com>
2018-01-22 18:05:29 +0100

    test page for jan mark

---
+++ src/components/Testpage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    Test page: {{myVar}}
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        myVar: "my string"
+      };
+    },
+    created: function() {
+    }
+  };
+</script>
+<style scoped>
+.container {
+  width: 800px;
+  padding: 50px;
+}
+</style>
--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,8 @@ import Collections from './components/collection/Collections.vue';
 import CollectionDetail from './components/collection/CollectionDetail.vue';
 import TermDetail from './components/term/TermDetail.vue';
 import NewTerm from './components/term/newTerm.vue';
-
 import Login from './components/Login.vue';
+import TestPage from './components/Testpage.vue';
 
 export const routes = [
     { path: '/', component: Home, name: 'Home' },
@@ -15,5 +15,6 @@ export const routes = [
     { path: '/ontologies/:id',  name:'ontologiesdetail'},
     { path: '/collections/:id', component: CollectionDetail, name: 'collectionDetail' },
     { path: '/new_term/:id', component: NewTerm, name: 'newTerm' },
-    { path: '/terms/:id', component: TermDetail, name: 'termDetail' }
+    { path: '/terms/:id', component: TermDetail, name: 'termDetail' },
+    { path: '/Testpage',  component: TestPage, name:'testpage'}
 ];
